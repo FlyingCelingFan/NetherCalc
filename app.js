d747db22987ac7cdb448aafa8c961aa08013e6bd
FlyingCelingFan <57369474+FlyingCelingFan@users.noreply.github.com>
2020-07-25 22:07:17 -1000

    Finilized javascript functions

--- app.js
+++ app.js
@@ -1,5 +1,16 @@
-function OvToNe() {
-    document.getElementById("oX").value;
-    document.getElementById("oY").value;
-    document.getElementById("oZ").value;
+//converts overworld coordinates to nether coordiantes
+function ovToNe() {
+    //multiplies overworld coords by 8 then changes nether coords accordingly
+    document.nethToOver.nX.value = document.overToNeth.oX.value / 8;
+    document.nethToOver.nY.value = document.overToNeth.oY.value / 8;
+    document.nethToOver.nZ.value = document.overToNeth.oZ.value / 8;
 }
+
+//converts overworld coordinates to nether coordiantes
+function neToOv() {
+    //multiplies overworld coords by 8 then changes nether coords accordingly
+    document.overToNeth.oX.value = document.nethToOver.nX.value * 8;
+    document.overToNeth.oY.value = document.nethToOver.nY.value * 8;
+    document.overToNeth.oZ.value = document.nethToOver.nZ.value * 8;
+}
+
